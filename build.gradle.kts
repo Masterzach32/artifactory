a8f42bbd0f62af670a2e63155b233ed817b4a23a
Zach Kozar <zachkozar@vt.edu>
2021-08-11 20:44:22 -0400

    Added release plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,13 @@ plugins {
     `kotlin-dsl`
     `maven-publish`
     id("com.gradle.plugin-publish") version "0.15.0"
+    id("net.researchgate.release") version "2.8.1"
 }
 
 group = "com.spicymemes"
-version = "0.1.4"
+
+val isRelease = !version.toString().endsWith("-SNAPSHOT")
+val isSnapshot = !isRelease
 
 repositories {
     mavenCentral()
@@ -17,8 +20,8 @@ repositories {
 
 dependencies {
     implementation(gradleKotlinDsl())
-    implementation("net.minecraftforge.gradle:ForgeGradle:5.1.+")
-    implementation("fabric-loom:fabric-loom.gradle.plugin:0.9.+")
+//    implementation("net.minecraftforge.gradle:ForgeGradle:5.1.+")
+//    implementation("fabric-loom:fabric-loom.gradle.plugin:0.9.+")
 }
 
 gradlePlugin {
@@ -36,4 +39,33 @@ pluginBundle {
     website = "https://github.com/SummerModVenture"
     vcsUrl = "https://github.com/SummerModVenture/artifactory"
     tags = listOf("minecraft")
+}
+
+tasks.publishPlugins {
+    onlyIf { isRelease }
+}
+
+publishing {
+    repositories {
+        if (isSnapshot) {
+            val mavenUsername: String? by project
+            val mavenPassword: String? by project
+            if (mavenUsername != null && mavenPassword != null) {
+                maven {
+                    name = "Snapshots"
+                    url = uri("https://maven.masterzach32.net/artifactory/gradle-plugins/")
+                    credentials {
+                        username = mavenUsername
+                        password = mavenPassword
+                    }
+                }
+            }
+        }
+    }
+}
+
+release {
+    preTagCommitMessage = "Release version"
+    tagCommitMessage = "Release version"
+    newVersionCommitMessage = "Next development version"
 }
--- src/main/kotlin/RunConfigs.kt
+++
@@ -1,9 +0,0 @@
-package com.spicymemes.artifactory
-
-import net.minecraftforge.gradle.common.util.*
-
-var RunConfig.forgeLoggingMarkers: List<String>
-    get() = properties["forge.logging.markers"]?.split(",") ?: emptyList()
-    set(value) {
-        properties["forge.logging.markers"] = value.joinToString(separator = ",")
-    }
